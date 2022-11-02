f7869c9f3faba080d95faf943d956f29d736d1fc
SSNmeister <shawn.wayne.jcrc12@gmail.com>
2022-11-02 22:58:53 +0800

    Created handyman HomePage

--- src/Home/HomePageHandyman.js
+++ src/Home/HomePageHandyman.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 const HomePageHandyman = () => {
-  return <div></div>;
+  return (
+    <div className="home--page--container">
+      <div className="home--page--top--section">
+        <p className="home--page--header">
+          You have not created any <span>services</span> yet
+        </p>
+        <p className="home--page--sub--header">Start creating now.</p>
+      </div>
+      <div className="home--page--create--section">
+        <button className="home--page--create--button br4 fs24 fw700">
+          Create
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default HomePageHandyman;
--- src/Home/HomePageMain.js
+++ src/Home/HomePageMain.js
@@ -1,12 +1,14 @@
 import React from "react";
+import Navbar from "../Components/Navbar";
 import HomePage from "./HomePage";
 import HomePageHandyman from "./HomePageHandyman";
 
 const HomePageMain = () => {
   return (
     <div>
+      <Navbar />
       <HomePage />
-      {/* <HomePageHandyman /> */}
+      <HomePageHandyman />
     </div>
   );
 };
